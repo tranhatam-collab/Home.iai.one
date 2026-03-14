18dc8e7c9ee00c068c6c0edca30663625a887462
tranhatam-collab <tranhatam@gmail.com>
2026-03-14 20:28:41 +0700

    Create en.js

---
+++ lang/en.js
@@ -0,0 +1,24 @@
+const LANG_EN = {
+
+hero_title:
+"Human Intelligence Infrastructure for the AI Age",
+
+hero_desc:
+"IAI.ONE is an ecosystem of tools, knowledge and communities designed to help humans thrive in the age of artificial intelligence.",
+
+ecosystem_title:
+"IAI Ecosystem",
+
+architecture_title:
+"AGI Architecture",
+
+civilization_title:
+"Civilization Operating System",
+
+platforms_title:
+"Platforms",
+
+philosophy_title:
+"System Philosophy"
+
+}
